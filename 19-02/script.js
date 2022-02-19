d790f20861fc5c296d85a6d98691c4ba4d1a81db
Elina <elnaibraimova18@gmail.com>
2022-02-19 21:21:49 +0600

    init:start tasks

---
+++ 19-02/script.js
@@ -0,0 +1,28 @@
+/*alert('Hello, world' );
+alert('Hello, world 2');*/
+
+// alert('сейчас будет ошибка ');
+// [1,2,3].forEach(alert)
+
+/*
+let name='Elina';
+alert(name);
+*/
+
+/*let age=99;
+age=55;
+alert(age);*/
+
+//let name='Elina'
+
+/*
+let return5=66;
+alert(return5);
+*/
+
+const nameUser='Elina';
+nameUser='Ibraimova';
+alert(nameUser);
+
+//Однострочный комментарий -CTRL+/
+/*мнготрочный комментарий  CTRL+SHIFT+/ */
